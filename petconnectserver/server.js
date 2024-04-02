13594f13afd36c043549fb56a005f638fce83505
joshua <joshuakpenhaligon@gmail.com>
2024-04-02 17:17:33 +0100

    added some comments

--- petconnectserver/server.js
+++ petconnectserver/server.js
@@ -242,17 +242,16 @@ app.post('/BookMinder', (req, res)=> {
 // VIEW PROFILE ROUTE
 
 app.get('/ViewProfile/:userId', (req, res) => {
-  // Note: Ensure the parameter name matches what's in the URL
   const { userId } = req.params; 
 
-  // SQL query to select specific fields from the users table
+  // the sql statement
   const sql = `
     SELECT username, email, phoneNumber, firstName, lastName, biography, accountStatus, role
     FROM users
     WHERE id = ?
   `;
 
-  // Execute the SQL query
+  // execute the sql statement and return it back to the page
   db.all(sql, [userId], (err, rows) => {
     if (err) {
       console.error('Error:', err.message);
